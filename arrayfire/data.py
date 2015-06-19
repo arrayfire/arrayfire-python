f3824198062699339d2eb83ae549d46631ca946e
Pavan Yalamanchili <pavan@arrayfire.com>
2015-06-19 15:32:13 -0400

    Generalizing af.constant to handle complex and 64 bit data types

--- arrayfire/data.py
+++ arrayfire/data.py
@@ -42,6 +42,22 @@ def constant(val, d0, d1=None, d2=None, d3=None, dtype=f32):
 
     out = array()
     dims = dim4(d0, d1, d2, d3)
-    c_val = c_double(val)
-    clib.af_constant(pointer(out.arr), c_val, 4, pointer(dims), dtype)
+
+    if isinstance(val, complex):
+        c_real = c_double(val.real)
+        c_imag = c_double(val.imag)
+
+        if (dtype != c32 and dtype != c64):
+            dtype = c32
+
+        clib.af_constant_complex(pointer(out.arr), c_real, c_imag, 4, pointer(dims), dtype)
+    elif dtype == s64:
+        c_val = c_longlong(val.real)
+        clib.af_constant_long(pointer(out.arr), c_val, 4, pointer(dims))
+    elif dtype == u64:
+        c_val = c_ulonglong(val.real)
+        clib.af_constant_ulong(pointer(out.arr), c_val, 4, pointer(dims))
+    else:
+        c_val = c_double(val)
+        clib.af_constant(pointer(out.arr), c_val, 4, pointer(dims), dtype)
     return out
