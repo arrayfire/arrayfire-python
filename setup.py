d88f25122399fbfd1bf20d99cd6f1b3e610b8dcb
Pavan Yalamanchili <pavan@arrayfire.com>
2015-08-28 14:54:54 -0400

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+#######################################################
+# Copyright (c) 2015, ArrayFire
+# All rights reserved.
+#
+# This file is distributed under 3-clause BSD license.
+# The complete license agreement can be obtained at:
+# http://arrayfire.com/licenses/BSD-3-Clause
+########################################################
+
+from setuptools import setup, find_packages
+
+## TODO:
+## 1) Look for af libraries during setup
+## 2) Include test suite
+
+setup(
+    name="arrayfire",
+    version="3.0.0",
+    description="Python bindings for ArrayFire",
+    license="BSD",
+    url="http://arrayfire.com",
+    packages=find_packages(exclude=['examples', 'tests']),
+)
