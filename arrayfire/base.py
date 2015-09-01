cef85eefe8e34dd6f0ca121fbd56aaae08f0123b
Pavan Yalamanchili <pavan@arrayfire.com>
2015-09-01 19:07:10 -0400

    Adding documentation for base.py and blas.py

--- arrayfire/base.py
+++ arrayfire/base.py
@@ -6,9 +6,17 @@
 # The complete license agreement can be obtained at:
 # http://arrayfire.com/licenses/BSD-3-Clause
 ########################################################
+
+"""
+Implementation of BaseArray class.
+"""
+
 from .library import *
 from .util import *
 
 class BaseArray(object):
+    """
+    Base array class for arrayfire. For internal use only.
+    """
     def __init__(self):
         self.arr = ct.c_void_p(0)
