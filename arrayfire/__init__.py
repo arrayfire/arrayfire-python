38f79160cbc442535246f5cdb98e3a7681564ebb
Pavan Yalamanchili <pavan@arrayfire.com>
2015-09-01 15:29:46 -0400

    Adding documentation for __init__.py

--- arrayfire/__init__.py
+++ arrayfire/__init__.py
@@ -7,6 +7,34 @@
 # http://arrayfire.com/licenses/BSD-3-Clause
 ########################################################
 
+"""
+A high performance scientific computing library for CUDA, OpenCL and CPU devices.
+
+The functionality provided by ArrayFire spans the following domains:
+
+    1. Vector Algorithms
+    2. Image Processing
+    3. Signal Processing
+    4. Computer Vision
+    5. Linear Algebra
+    6. Statistics
+
+Programs written using ArrayFire are portable across CUDA, OpenCL and CPU devices
+
+The default backend is chosen in the following order of preference based on the available libraries:
+
+    1. CUDA
+    2. OpenCL
+    3. CPU
+
+The backend can be chosen at the beginning of the program by using the following function
+
+    >>> af.backend.set(name)
+
+where name is one of 'cuda', 'opencl' or 'cpu'
+
+"""
+
 from .library    import *
 from .array      import *
 from .data       import *
