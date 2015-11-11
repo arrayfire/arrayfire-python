2db978daea859323d7c32799b4ae8c632df10ab3
Pavan Yalamanchili <pavan@arrayfire.com>
2015-11-11 13:46:32 -0500

    Updating version information

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     author="Pavan Yalamanchili",
     author_email="pavan@arrayfire.com",
     name="arrayfire",
-    version="3.1.0",
+    version="3.2.0",
     description="Python bindings for ArrayFire",
     license="BSD",
     url="http://arrayfire.com",
