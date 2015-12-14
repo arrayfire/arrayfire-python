36fd392d3ca1dc7e3cdd520a6fa4b0603db4106b
Pavan Yalamanchili <pavan@arrayfire.com>
2015-12-14 18:26:43 -0500

    Updating CHANGELOG and the version number

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     author="Pavan Yalamanchili",
     author_email="pavan@arrayfire.com",
     name="arrayfire",
-    version="3.2.20151211",
+    version="3.2.20151214",
     description="Python bindings for ArrayFire",
     license="BSD",
     url="http://arrayfire.com",
