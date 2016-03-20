ced0322386f1ad46dcf7d8ebef39258312eaa737
Pavan Yalamanchili <pavan@arrayfire.com>
2016-03-20 02:25:05 -0400

    FEAT: Adding functions to interact with other opencl libraries

--- arrayfire/opencl.py
+++ arrayfire/opencl.py
@@ -13,6 +13,30 @@ Functions specific to OpenCL backend.
 This module provides interoperability with other OpenCL libraries.
 """
 
+from .util import *
+from .library import (_Enum, _Enum_Type)
+
+class DEVICE_TYPE(_Enum):
+    """
+    ArrayFire wrapper for CL_DEVICE_TYPE
+    """
+    CPU = _Enum_Type(1<<1)
+    GPU = _Enum_Type(1<<2)
+    ACC = _Enum_Type(1<<3)
+    UNKNOWN = _Enum_Type(-1)
+
+class PLATFORM(_Enum):
+    """
+    ArrayFire enum for common platforms
+    """
+    AMD     = _Enum_Type(0)
+    APPLE   = _Enum_Type(1)
+    INTEL   = _Enum_Type(2)
+    NVIDIA  = _Enum_Type(3)
+    BEIGNET = _Enum_Type(4)
+    POCL    = _Enum_Type(5)
+    UNKNOWN = _Enum_Type(-1)
+
 def get_context(retain=False):
     """
     Get the current OpenCL context being used by ArrayFire.
@@ -107,3 +131,87 @@ def set_device_id(idx):
 
     safe_call(backend.get().afcl_set_device_id(idx))
     return
+
+def add_device_context(dev, ctx, que):
+    """
+    Add a new device to arrayfire opencl device manager
+
+    Parameters
+    ----------
+
+    dev : cl_device_id
+
+    ctx : cl_context
+
+    que : cl_command_queue
+
+    """
+    if (backend.name() != "opencl"):
+        raise RuntimeError("Invalid backend loaded")
+
+    safe_call(backend.get().afcl_add_device_context(dev, ctx, que))
+
+def set_device_context(dev, ctx):
+    """
+    Set a device as current active device
+
+    Parameters
+    ----------
+
+    dev  : cl_device_id
+
+    ctx  : cl_context
+
+    """
+    if (backend.name() != "opencl"):
+        raise RuntimeError("Invalid backend loaded")
+
+    safe_call(backend.get().afcl_set_device_context(dev, ctx))
+
+def delete_device_context(dev, ctx):
+    """
+    Delete a device
+
+    Parameters
+    ----------
+
+    dev  : cl_device_id
+
+    ctx  : cl_context
+
+    """
+    if (backend.name() != "opencl"):
+        raise RuntimeError("Invalid backend loaded")
+
+    safe_call(backend.get().afcl_delete_device_context(dev, ctx))
+
+
+_to_device_type = {DEVICE_TYPE.CPU.value     : DEVICE_TYPE.CPU,
+                   DEVICE_TYPE.GPU.value     : DEVICE_TYPE.GPU,
+                   DEVICE_TYPE.ACC.value     : DEVICE_TYPE.ACC,
+                   DEVICE_TYPE.UNKNOWN.value : DEVICE_TYPE.UNKNOWN}
+
+_to_platform    = {PLATFORM.AMD.value     : PLATFORM.AMD,
+                   PLATFORM.APPLE.value   : PLATFORM.APPLE,
+                   PLATFORM.INTEL.value   : PLATFORM.INTEL,
+                   PLATFORM.NVIDIA.value  : PLATFORM.NVIDIA,
+                   PLATFORM.BEIGNET.value : PLATFORM.BEIGNET,
+                   PLATFORM.POCL.value    : PLATFORM.POCL,
+                   PLATFORM.UNKNOWN.value : PLATFORM.UNKNOWN}
+
+
+def get_device_type():
+    """
+    Get opencl device type
+    """
+    res = ct.c_int(DEVICE_TYPE.UNKNOWN.value)
+    safe_call(backend.get().afcl_get_device_type(ct.pointer(res)))
+    return _to_device_type[res.value]
+
+def get_platform():
+    """
+    Get opencl platform
+    """
+    res = ct.c_int(PLATFORM.UNKNOWN.value)
+    safe_call(backend.get().afcl_get_platform(ct.pointer(res)))
+    return _to_platform[res.value]
